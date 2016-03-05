c3f113ffc1b3aee62314612a5892150083605981
MusayevKamran <mk-cce@mail.ru>
2016-03-05 14:45:50 +0300

    add main method

--- model-3d/src/main/java/ru/pfur/skis/model/TestClass.java
+++ model-3d/src/main/java/ru/pfur/skis/model/TestClass.java
@@ -4,4 +4,7 @@ package ru.pfur.skis.model;
  * Created by Art on 05.03.16.
  */
 public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello GIT");
+    }
 }
