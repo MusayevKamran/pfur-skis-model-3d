3b8960dcd4313800aa3792f07a53bb4ccd2c718f
Kamran <mk-cce@mail.ru>
2016-05-07 22:27:27 +0300

    open frame by clickin node and bar

--- src/main/java/ru/pfur/skis/ui/CreateBar.java
+++ src/main/java/ru/pfur/skis/ui/CreateBar.java
@@ -1,28 +1,88 @@
 package ru.pfur.skis.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Kamran on 5/7/2016.
  */
 public class CreateBar extends JFrame{
 
+    private JButton ADD;
+    private JButton APPLY;
+    private JButton CLOSE;
+
+    private JLabel labelX1;
+    private JLabel labelX2;
+    private JLabel labelY1;
+    private JLabel labelY2;
+    private JLabel labelZ1;
+    private JLabel labelZ2;
+
+    private JTextField getX1 = new JTextField();
+    private JTextField getX2 = new JTextField();
+    private JTextField getY1 = new JTextField();
+    private JTextField getY2 = new JTextField();
+    private JTextField getZ1 = new JTextField();
+    private JTextField getZ2 = new JTextField();
+
+    public CreateBar(String s) throws HeadlessException {
+        super(s);
+        setTitle("Bar");
+        setVisible(true);
+        setAlwaysOnTop(true);
+        setSize(new Dimension(300, 310));
+        setLayout(new FlowLayout());
+
+        labelX1 = new JLabel("X 1");
+        labelX1.setPreferredSize(new Dimension(100, 30));
+        this.add(labelX1);
+        getX1.setPreferredSize(new Dimension(100, 30));
+        this.add(getX1);
+
+        labelX2 = new JLabel("X 1");
+        labelX2.setPreferredSize(new Dimension(100, 30));
+        this.add(labelX2);
+        getX2.setPreferredSize(new Dimension(100, 30));
+        this.add(getX2);
+
+        labelY1 = new JLabel("Y 1");
+        labelY1.setPreferredSize(new Dimension(100, 30));
+        this.add(labelY1);
+        getY1.setPreferredSize(new Dimension(100, 30));
+        this.add(getY1);
+
+        labelY2 = new JLabel("Y 2");
+        labelY2.setPreferredSize(new Dimension(100, 30));
+        this.add(labelY2);
+        getY2.setPreferredSize(new Dimension(100, 30));
+        this.add(getY2);
+
+        labelZ1 = new JLabel("Z 1");
+        labelZ1.setPreferredSize(new Dimension(100, 30));
+        this.add(labelZ1);
+        getZ1.setPreferredSize(new Dimension(100, 30));
+        this.add(getZ1);
+
+        labelZ2 = new JLabel("Z 2");
+        labelZ2.setPreferredSize(new Dimension(100, 30));
+        this.add(labelZ2);
+        getZ2.setPreferredSize(new Dimension(100, 30));
+        this.add(getZ2);
+
+
+        ADD = new JButton("  ADD  ");
+        APPLY = new JButton("APPLY");
+        CLOSE = new JButton("CLOSE");
+
+        this.add(ADD);
+        this.add(APPLY);
+        this.add(CLOSE);
+
+    }
+
     public static void main(String[] args) {
-    JFrame frame = new JFrame("Bar");
-    JPanel panel = new JPanel();
-    frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-    frame.setSize(300,150);
-    frame.setVisible(true);
-
-    JTextArea getX = new JTextArea();
-    JTextArea getY = new JTextArea();
-    JTextArea getZ = new JTextArea();
-
-    panel.add(getX);
-    panel.add(getY);
-    panel.add(getZ);
-
-    frame.add(panel);
-}
-
+        CreateBar frame = new CreateBar("Bar");
+        frame.setVisible(true);
+    }
 }
--- src/main/java/ru/pfur/skis/ui/CreateNode.java
+++ src/main/java/ru/pfur/skis/ui/CreateNode.java
@@ -1,8 +1,58 @@
 package ru.pfur.skis.ui;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Kamran on 5/7/2016.
  */
-public class CreateNode {
+public class CreateNode extends JFrame {
+    private JButton ADD;
+    private JButton APPLY;
+    private JButton CLOSE;
+    private JLabel labelX;
+    private JLabel labelY;
+    private JLabel labelZ;
 
+    private JTextField getX = new JTextField();
+    private JTextField getY = new JTextField();
+    private JTextField getZ = new JTextField();
+
+    public CreateNode(String s) throws HeadlessException {
+        super(s);
+        setTitle("Node");
+        setVisible(true);
+        setAlwaysOnTop(true);
+        setSize(new Dimension(300, 200));
+        setLayout(new FlowLayout());
+        labelX = new JLabel("X");
+        labelX.setPreferredSize(new Dimension(100, 30));
+        this.add(labelX);
+        getX.setPreferredSize(new Dimension(100, 30));
+        this.add(getX);
+        labelY = new JLabel("Y");
+        labelY.setPreferredSize(new Dimension(100, 30));
+        this.add(labelY);
+        getY.setPreferredSize(new Dimension(100, 30));
+        this.add(getY);
+        labelZ = new JLabel("Z");
+        labelZ.setPreferredSize(new Dimension(100, 30));
+        this.add(labelZ);
+        getZ.setPreferredSize(new Dimension(100, 30));
+        this.add(getZ);
+
+        ADD = new JButton("  ADD  ");
+        APPLY = new JButton("APPLY");
+        CLOSE = new JButton("CLOSE");
+
+        this.add(ADD);
+        this.add(APPLY);
+        this.add(CLOSE);
+
+    }
+
+    public static void main(String[] args) {
+        CreateNode frame = new CreateNode("Node");
+        frame.setVisible(true);
+    }
 }
